7ae83a1e81faea1262bcfb7bfd805da95a4e1999
joeynkay <cyou1029122023@gmail.com>
2020-07-30 02:13:30 +0800

    初始化一个基础的scroll组件，用于页面滚动

--- src/components/common/Scroll.vue
+++ src/components/common/Scroll.vue
@@ -7,7 +7,6 @@
 
 <script>
 import BScroll from 'better-scroll'
-
  
 export default {
   name: 'Scroll',
@@ -66,40 +65,6 @@ export default {
         click: this.click,
         scrollX: this.scrollX
       })
-
-      // 是否派发滚动事件
-      if (this.listenScroll) {
-        this.scroll.on('scroll', (pos) => {
-          this.$emit('scroll', pos)
-        })
-      }
-
-      // 是否派发滚动到底部事件，用于上拉加载
-      if (this.pullup) {
-        this.scroll.on('scrollEnd', () => {
-          // 滚动到底部
-          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
-            this.$emit('scrollToEnd')
-          }
-        })
-      }
-
-      // 是否派发顶部下拉事件，用于下拉刷新
-      if (this.pulldown) {
-        this.scroll.on('touchend', (pos) => {
-          // 下拉动作
-          if (pos.y > 50) {
-            this.$emit('pulldown')
-          }
-        })
-      }
-
-      // 是否派发列表滚动开始的事件
-      if (this.beforeScroll) {
-        this.scroll.on('beforeScrollStart', () => {
-          this.$emit('beforeScroll')
-        })
-      }
     },
     disable() {
       // 代理better-scroll的disable方法
@@ -113,20 +78,12 @@ export default {
       // 代理better-scroll的refresh方法
       this.scroll && this.scroll.refresh()
     },
-    scrollTo() {
-      // 代理better-scroll的scrollTo方法
-      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
-    },
-    scrollToElement() {
-      // 代理better-scroll的scrollToElement方法
-      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
-    }
   },
   watch: {
     data () {
       setTimeout(() => {
           this.refresh()
-        }, this.refreshDelay)
+      }, this.refreshDelay)
     }
   }
 }
