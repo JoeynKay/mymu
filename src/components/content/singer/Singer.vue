42b7168c724b6da99ec79fdff594e8bd3d4971e8
joeynkay <cyou1029122023@gmail.com>
2020-07-30 15:35:34 +0800

    在singer页面获取了歌手列表接口的数据，并保存在了组件本地的data：singers中

--- src/components/content/singer/Singer.vue
+++ src/components/content/singer/Singer.vue
@@ -6,8 +6,31 @@
 </template>
 
 <script>
+import {getSingerList} from 'network/singer.js'
+import {ERR_OK} from 'network/config'
+
 export default {
   name: 'Singer',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    console.log('singer-created')
+    this._getSingerList()
+  },
+  methods: {
+    //抓取歌手数据接口
+    _getSingerList () {
+      getSingerList()
+        .then((res) => {
+          // console.log(res.data.list)
+          this.singers = res.data.list
+          console.log(this.singers)
+        })
+    }
+  }
 }
 </script>
 
