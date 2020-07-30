f34f212ee454bc6edcc8a261ce79ec984bb55534
joeynkay <cyou1029122023@gmail.com>
2020-07-30 12:12:30 +0800

    定制了一个loading组件 用于页面加载时，给网络卡的用户提供好的视觉体验 该组件开放了一个props：title，使开发者可以定制加载信息

---
+++ src/components/common/loading/Loading.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+  <div class="loading">
+    <img src="./loading.gif" width="24" height="24">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    title: {
+      type: String,
+      default: '正在载入...'
+    }
+  }
+}
+</script>
+
+<style  scoped lang="stylus">
+  @import "~assets/stylus/variable.styl"
+
+  .loading
+    text-align: center
+    .desc
+      color: $color-text
+      font-size: $font-size-small
+
+</style>
