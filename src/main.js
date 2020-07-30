f243297877431271de730939130d6a771a3c9259
joeynkay <cyou1029122023@gmail.com>
2020-07-30 12:14:15 +0800

    添加了fastclick插件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
 
 //引入样式
 import 'assets/stylus/index.styl'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
