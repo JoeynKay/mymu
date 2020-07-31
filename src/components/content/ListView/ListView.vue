53fc04c4a0c0c587b451062165715d08d7847019
joeynkay <cyou1029122023@gmail.com>
2020-08-01 00:12:15 +0800

    主列表和锚点栏的关联逻辑的实现

--- src/components/content/ListView/ListView.vue
+++ src/components/content/ListView/ListView.vue
@@ -1,6 +1,12 @@
 <!--  -->
 <template>
-  <scroll class="listview" :data="datalist" ref="listView">
+  <scroll class="listview" 
+          :data="datalist" 
+          ref="listView"
+          :listenScroll="listenScroll"
+          :probeType="probeType"
+          @scroll="scroll"
+  >
     <!-- 歌手列表 -->
     <ul>
       <li 
@@ -26,9 +32,9 @@
     <!-- 右侧快速入口 -->
     <div class="list-shorcut">
       <ul>
-        <li 
-            v-for="(item, index) in shortcutList" 
+        <li v-for="(item, index) in shortcutList" 
             class="item"
+            :class="{current:currentIndex === index}"
             :data-index="index"
             :key="index"
             @touchstart="onShortcutStart"
@@ -59,6 +65,12 @@ export default {
       })
     }
   },
+  data () {
+    return {
+      scrollY: -1,
+      currentIndex: 0
+    }
+  },
   props: {
     //传入的列表数据
     datalist: {
@@ -73,6 +85,9 @@ export default {
   },
   created () {
     this.touch = {}
+    this.listHeight = []
+    this.listenScroll = true
+    this.probeType = 3
   },
   methods: {
     onShortcutStart (e) {
@@ -81,7 +96,6 @@ export default {
        //使用公共变量存储，以便方法间通信
       this.touch.y1 = anchorTouch.pageY
       let anchorIndex = getData(e.target, 'index')
-      console.log(anchorIndex)
       this.touch.anchorIndex = anchorIndex
       this._scrollTo(anchorIndex)
     },
@@ -96,11 +110,80 @@ export default {
       let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
       // console.log(delta)
       let anchorIndex = this.touch.anchorIndex - 0 + delta
-      console.log(anchorIndex)
+      // console.log(anchorIndex)
       this._scrollTo(anchorIndex)
+      // console.log(this.scrollY)
     },
     _scrollTo (anchorIndex) {
+      //更新scrollY的数据，
+      // console.log(anchorIndex)
+      //this.listHeight[anchorIndex]存的是正值高度
+      //而this.scrollY在watch监测下是负值高度处理的
+      //也就是说两者互为相反值，取反，便是我们想要得到的this.scrollY
+      if (anchorIndex < 0) {
+        anchorIndex = 0
+      } else if (anchorIndex > this.listHeight.length - 2) {
+        anchorIndex = this.listHeight.length - 2
+      }
+      this.scrollY = -this.listHeight[anchorIndex]
       this.$refs.listView.scrollToElement(this.$refs.listGroup[anchorIndex], 0)
+    },
+    //获取滚动后的列表所处在的位置
+    //缓存于变量中
+    scroll (pos) {
+      this.scrollY = pos.y
+      // console.log(this.scrollY)
+    },
+    //计算每个group的高度，返回一个高度分布数组
+    //应该在有数据时更新
+    _calculateHeight () {
+      //每次都初始化数组，存储列表group的高度
+      this.listHeight = []
+      const list = this.$refs.listGroup
+      let height = 0
+      this.listHeight.push(height)
+      //获取dom列表在屏幕的高度
+      //存进高度分布数组中
+      for(let i = 0; i < list.length; i++) {
+        let item = list[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    }
+  },
+  watch: {
+    datalist () {
+      //当列表数据变化后，重新计算高度
+      // console.log('jisuangaodu')
+      setTimeout(() => {
+        this._calculateHeight()
+        // console.log('2')
+      }, 20)
+    },
+    scrollY (newY) {
+      //每次当前屏幕坐标变化后，对比高度分布数组，从而知道在哪个分区
+      //以便使锚点的样式为高亮
+      //缓存高度分布数组
+      const listHeight = this.listHeight
+      //当滚动到顶部，newY > 0
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      } 
+      //在中部滚动
+      for(let i = 0; i < listHeight.length - 1; i++) {
+        // console.log('11')
+        let height1 = listHeight[i]
+        let height2 = listHeight[i+1]
+        //如果到了最后，或者当前屏幕坐标位于高度分布数组的区间
+        if(-newY >= height1 && -newY < height2) {
+          //缓存当前的分区到data中的数据中
+          this.currentIndex = i
+          return
+        }
+      }
+      //当滚动到底部，且-newY大于最后一个元素的上限时
+      this.currentIndex = listHeight.length - 2
     }
   }
 }
@@ -152,7 +235,7 @@ export default {
         padding 3px
         line-height 1
         color $color-text-l
-        font-size $font-size-small
+        font-size $font-size-medium
         &.current
           color $color-theme
 
