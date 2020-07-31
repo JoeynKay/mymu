7310aa8abb11f100ecb5d79d033af6cb88049623
joeynkay <cyou1029122023@gmail.com>
2020-07-31 11:16:14 +0800

    目录结构调整

---
+++ src/views/recommend/Recommend.vue
@@ -0,0 +1,158 @@
+<!--  -->
+<template>
+  <div class="recommend">
+    <scroll ref="scroll" class="recommend-content">
+      <!-- 使用better-scroll整体管理推荐页面 -->
+      <div>
+        <div class="slider-wrapper" v-if="banner.length">
+        <slider>
+          <div v-for="item in banner" :key="item.actid">
+            <a :href="item.jumpurl">
+              <img class="needsclick" :src="item.picurl" @load="loadImage">
+            </a>
+          </div>
+      </slider>
+      </div>
+      <!-- 推荐歌单列表start -->
+      <div class="recommend-list">
+        <!-- 歌单列表标题 -->
+        <h1 class="list-title">推荐歌单列表</h1>
+        <!-- 歌单列表 -->
+        <ul>
+          <li v-for="item in discList" :key="item.dissid" class="item">
+            <div class="icon">
+              <img v-lazy="item.imgurl" width="60" height="60">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- 推荐歌单列表end -->
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import {getRecommend, getDiscList} from 'network/recommend'
+
+import Slider from 'common/slider/Slider'
+import Scroll from 'common/Scroll'
+import Loading from 'common/loading/Loading'
+
+export default {
+  name: 'Recommend',
+  beforeRouteLeave (to, from, next) {
+    // clearTimeout(this.timer)
+    console.log('route-leave-recommend')
+    // console.log(this.$refs.slider)
+    next()
+  },
+  beforeRouteEnter(to, from, next){
+    next(() => {
+      console.log('route-enter-recommend')
+    })
+  },
+  components: {
+    Slider,
+    Scroll,
+    Loading
+  },
+  data () {
+    return {
+      banner: [],
+      discList: []
+    }
+  },
+  created () {
+    this._getRecommend()
+    setTimeout(() => { 
+       console.log('开启了定时器，人工延时，显示loading.gif')
+       this._getDiscList()
+      }, 1000)
+   
+  },
+  destroyed () {
+    console.log('Recommend组件没有keep-alive')
+  },
+  methods: {
+    //获得轮播图数据
+    _getRecommend () {
+      getRecommend().then(res => {
+        // console.log(res.data.banner)//打印一下数据
+        this.banner = res.data.banner
+      })
+    },
+    //获得歌单数据
+    _getDiscList () {
+      getDiscList().then(res => {
+        // console.log(res)
+       this.discList = res.data.list
+      })
+    },
+    loadImage () {
+      if(!this.checkLoaded) {
+        // console.log('image')
+        this.$refs.scroll.refresh()
+      }
+    }
+  },
+ 
+}
+</script>
+
+<style  scoped lang="stylus">
+  @import "~assets/stylus/variable.styl"
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+</style>
---
+++ src/views/singer/Singer.vue
@@ -0,0 +1,98 @@
+<!--  -->
+<template>
+  <div class="singer">
+    <list-view :data="singers">
+
+    </list-view>
+  </div>
+</template>
+
+<script>
+import {getSingerList} from 'network/singer.js'
+import {ERR_OK} from 'network/config'
+import Singer from 'assets/js/singer'
+
+import ListView from 'content/ListView/ListView'
+
+//定义两个常量，存储热门歌曲的首数及其标题
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
+export default {
+  name: 'Singer',
+  components: {
+    ListView
+  },
+  data () {
+    return {
+      singers: [],
+    }
+  },
+  created () {
+    console.log('singer-created')
+    this._getSingerList()
+  },
+  methods: {
+    //抓取歌手数据接口
+    _getSingerList () {
+      getSingerList()
+        .then((res) => {
+          // console.log(res.data.list)
+          // this.singers = res.data.list
+          // console.log(this._normaliseSinger(this.singers))
+          this.singers = this._normaliseSinger(res.data.list)
+          console.log(this.singers)
+        })
+    },
+    //对获取的数据接口进行处理
+    //最后返回[{'title'@str, 'items'@[{'id'@str, 'name'@str, 'avatar'@modstr}]}, {}]
+    _normaliseSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      //遍历list中的每个元素，并进行对应处理，最终按照设计放进了map对象中
+      list.forEach((element,index) => {
+        if(index < HOT_SINGER_LEN) {
+          //缓存热门歌曲数组
+          map.hot.items.push(new Singer(element.Fsinger_mid, element.Fsinger_name))
+        }
+        //下面是常规歌手列表的初始化
+        //先缓存每个元素的Findex
+        const key = element.Findex
+        if (!map[key]) {
+          //如果map对象中还没有添加某Findex属性
+          map[key] = {
+            title: key,
+            items: []//也是对应的某字母开头的歌手列表
+          }
+        }
+        map[key].items.push(new Singer(element.Fsinger_mid, element.Fsinger_name))
+      })
+      //再对map对象进行处理，排好顺序，以获得正常的序列
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if(val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      //ret数组中的排序
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+
+      return hot.concat(ret)
+    }
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
